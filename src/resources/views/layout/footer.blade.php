247215a8d2954d8c7d0545b1959ea8f2368c153a
rlawotjd <rlawotjd026@gmail.com>
2023-06-13 17:16:45 +0900

    0613 (src 공동페이지 udt) 김재성

--- src/resources/views/layout/footer.blade.php
+++ src/resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer mt-auto py-3 bg-light mt-auto">
     <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <span class="text-muted">가원이의 축제</span>
     </div>
 </footer>
--- src/resources/views/layout/layout.blade.php
+++ src/resources/views/layout/layout.blade.php
@@ -8,15 +8,10 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
     @include('layout.header')
     @yield('content')
-    {{-- @include('layout.footer') --}}
-    <footer class="footer mt-auto py-3 bg-light">
-        <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
-        </div>
-    </footer>
+    @include('layout.footer')
 
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
