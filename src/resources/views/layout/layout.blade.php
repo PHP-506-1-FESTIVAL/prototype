61a51bcfe355fac0f6506aee0de348a94f8c6014
rlawotjd <rlawotjd026@gmail.com>
2023-06-14 10:21:10 +0900

    0614 (src 공동페이지 udt) 김재성

--- src/resources/views/layout/layout.blade.php
+++ src/resources/views/layout/layout.blade.php
@@ -8,9 +8,11 @@
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body class="d-flex flex-column min-vh-100">
-    @include('layout.header')
-    @yield('content')
+<body class="d-flex flex-column min-vh-100 ">
+        @include('layout.header')
+    <div class="mx-5 my-5">
+        @yield('content')
+    </div>
     @include('layout.footer')
 
     <!-- JavaScript Bundle with Popper -->
