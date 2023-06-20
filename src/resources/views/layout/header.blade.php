2a24f21e84c975d4a4152fba037acfda4ec45eeb
rlawotjd <rlawotjd026@gmail.com>
2023-06-20 10:36:32 +0900

    0620 (공통 페이지 업데이트) 김재성

--- src/resources/views/layout/header.blade.php
+++ src/resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
         @endauth
 
         @guest --}}
-            <a class="navbar-brand" href="{{route('main.view')}}">마실가실?</a>
+            <a class="navbar-brand" href="{{route('main')}}">마실가실?</a>
         {{-- @endguest --}}
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
