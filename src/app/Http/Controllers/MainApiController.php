6894e9a27deccdad8cd1d8e729e778fad0f05f5e
rlawotjd <rlawotjd026@gmail.com>
2023-06-20 21:04:33 +0900

    0620 (메인에피아이 업데이트) 김재성

--- src/app/Http/Controllers/MainApiController.php
+++ src/app/Http/Controllers/MainApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Festival;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 
 class MainApiController extends Controller
 {
@@ -12,15 +13,32 @@ class MainApiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($area_code)
+    public function index($str_val)
     {
         //축제 지역/월 별 출력
+        $arr_temp=explode(',',$str_val);
+        // var_dump($arr_temp);
+        $arr_val=['area_code'=>$arr_temp[0],'month'=>Date("Y-".sprintf('%02d',$arr_temp[1])."-d",time())];
+        // var_dump($month);
         // $fes_info=Festival::all();
-        $fes_info=Festival::select([
-            'festival_id','festival_title', 'festival_start_date', 'festival_end_date', 'area_code', 'poster_img', 'festival_hit', 'festival_state'
-        ])->where('area_code',$area_code)
-        // ->where('festival_end_date','<',$month)
-        ->orderBy('festival_hit')->limit(4)->get();
+        // var_dump($arr_val);
+        if ($arr_temp[1]==="") {
+            $arr_val['month']=DATE("Y-m-d",time());
+        }
+        // $fes_info=Festival::select([
+            //     'festival_id','festival_title', 'festival_start_date', 'festival_end_date', 'area_code', 'poster_img', 'festival_hit', 'festival_state'
+            // ])->where('area_code',$arr_val['area_code'])
+            // ->where('festival_end_date','<',$arr_val['month'])
+            // // ->where('festival_end_date','<',$month)
+            // ->orderBy('festival_hit')->limit(4)->get();
+            $fes_temp=Festival::select([
+                'festival_id','festival_title', 'festival_start_date', 'festival_end_date', 'area_code', 'poster_img', 'festival_hit', 'festival_state'
+                ])->where('festival_end_date','<',$arr_val['month']);
+                // ->where('festival_end_date','<',$month)
+                if ($arr_temp[0]!=="") {
+                    $fes_temp->where('area_code',$arr_val['area_code']);
+                }
+                $fes_info=$fes_temp->orderBy('festival_hit')->limit(4)->get();
 
         return $fes_info;
     }
