34bc7dc1ebf7f9285647d3002119dbf4e8beae66
rlawotjd <rlawotjd026@gmail.com>
2023-06-22 15:51:50 +0900

    0621 (메인에피아이 업데이트) 김재성

--- src/app/Http/Controllers/MainApiController.php
+++ src/app/Http/Controllers/MainApiController.php
@@ -17,21 +17,25 @@ class MainApiController extends Controller
     // 축제 지역 및 월 분리
     $arr_temp = explode(',', $str_val);
 
+    if ($arr_temp[1]==="") {
+        $arr_temp[1]=Date('m',time());
+    }
     $arr_val = [
         'area_code' => $arr_temp[0], // 축제 지역 코드
         'month' => sprintf('%02d', $arr_temp[1]), // 선택된 월
     ];
 
-    $fes_info = Festival::select([
+    $fes_temp = Festival::select([
         'festival_id', 'festival_title', 'festival_start_date', 'festival_end_date', 'area_code', 'poster_img', 'festival_hit', 'festival_state'
-    ])
-    ->where('area_code', $arr_val['area_code'])
-    ->where(function ($query) use ($arr_val) {
+    ]);
+    if ($arr_temp[0]!=="") {
+        $fes_info=$fes_temp ->where('area_code', $arr_val['area_code']);
+    }
+    $fes_info=$fes_temp->where(function ($query) use ($arr_val) {
         $query->whereRaw('MONTH(festival_start_date) = ?', [$arr_val['month']]) // 축제 시작 날짜의 월과 선택된 월이 일치하는 경우
         ->orWhereRaw('MONTH(festival_end_date) = ?', [$arr_val['month']]); // 축제 종료 날짜의 월과 선택된 월이 일치하는 경우
     })
     ->orderBy('festival_hit')
-    ->limit(4)
     ->get();
 
     return $fes_info;
