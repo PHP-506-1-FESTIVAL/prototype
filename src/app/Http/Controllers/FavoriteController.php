5272ae964438edf9c381c5f09621b36cd177e87d
rlawotjd <rlawotjd026@gmail.com>
2023-06-22 10:42:22 +0900

    0621 (찜 업데이트) 김재성

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -9,6 +9,9 @@ class FavoriteController extends Controller
 {
     public function jjmPost(Request $req)
     {
+        if(auth()->guest()) {
+            return redirect()->route('user.login');
+        }
         // dump($req);
         $arr_req=explode(',',$req->test);
         $fav=new Favorite(['user_id'=>$arr_req[0]
