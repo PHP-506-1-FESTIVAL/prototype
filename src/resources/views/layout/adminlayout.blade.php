105d8d8a2c8790ea30c7bc40d1386f4a4f2e81e3
xellos88 <parkjy0419@naver.com>
2023-07-17 18:32:22 +0900

    0717 (src 관리자로그인 udt) 박진영

--- src/resources/views/layout/adminlayout.blade.php
+++ src/resources/views/layout/adminlayout.blade.php
@@ -212,13 +212,13 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ session('admin_name') }}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>{{ session('admin_name') }}</h6>
+              <span>관리자 등급 자리</span>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -227,7 +227,7 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
-                <span>My Profile</span>
+                <span>마이프로필</span>
               </a>
             </li>
             <li>
