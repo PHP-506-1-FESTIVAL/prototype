dd62653e0a639ae87085e4ef70c6975be7c9c641
leegawon0 <kaepang07@naver.com>
2023-07-17 12:22:58 +0900

    0717 (관리자페이지 레이아웃) 이가원

--- src/resources/views/layout/adminlayout.blade.php
+++ src/resources/views/layout/adminlayout.blade.php
@@ -44,7 +44,7 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="{{route('admin.main')}}" class="logo d-flex align-items-center">
         <img src="/assets/images/favicon.png" alt="">
         <span class="d-none d-lg-block">MSGS Admin</span>
       </a>
