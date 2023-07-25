a2df4de3746ee91e4a276d47eb6b7485bb3ed938
xellos88 <parkjy0419@naver.com>
2023-07-25 16:02:34 +0900

    0725 (src 리뷰 udt)박진영

--- src/resources/views/festival_detail.blade.php
+++ src/resources/views/festival_detail.blade.php
@@ -338,8 +338,14 @@ height: 100%;
                             @endforeach
                             <p class="meta">닉네임: {{ $review->user_nickname }} 작성일: {{ $review->updated_at }}</p>
                             <p style="padding:0 0 10px;">{{ $review->review_content }}</p>
+                                                                                                @if(session('user_id') != $review->user_id)
+                                                <a href="javascript:popup()">
+                                                    <i class="lni lni-alarm"></i>
+                                                    신고하기
+                                                </a>
+                                        @endif
                             <div class="col-4" style="padding:0;">
-
+                        
                                 <div class="button-group" style="display: flex; gap: 5px;">
                                 <form action="{{ route('review.delete', ['id' => $review->review_id]) }}" method="post" name="removefrm_{{ $review->review_id }}">
                                     @csrf
@@ -819,6 +825,14 @@ height: 100%;
         })
     }
 </script>
+{{-- <script>
+                function popup(){
+            var url = "{!! route('insert.report', ['no' => $reviews->review_id, 'type' => '1']) !!}";
+            var name = "popup test";
+            var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+            window.open(url, name, option);
+        }
+</script> --}}
 </section>
 {{-- <script src="{{asset('js/festival.js')}}"></script> --}}
 @endsection
